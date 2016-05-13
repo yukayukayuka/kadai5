7defec7a190f266b9e6890bc77bff1de5ddcac6f
g14954wy <g14954wy@s30424.tsuda.ac.jp>
2016-05-13 11:34:34 +0900

    ターン１

--- kadai5/src/kadai5/IntToEng.java
+++ kadai5/src/kadai5/IntToEng.java
@@ -3,6 +3,11 @@ package kadai5;
 import java.util.Scanner;
 
 public class IntToEng {
+	static String num="";
+	static String numbers1[]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+	static String numberTeen[]={"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+	static String numbers10[]={"","","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);	
         	int input = sc.nextInt();	
@@ -11,6 +16,34 @@ public class IntToEng {
         
     }
 	static String translateEng(int n) {
-		return "";
+		if(n<=99)return underTwo(n);
+		else {
+			int shou100=n/100;
+			int amari100=n%100;//amari100は下二桁
+			num=numbers1[shou100]+" hundred and "+underTwo(amari100);
+			return num;
+		}
+
+    }
+	static String underTwo(int n){//nは二桁の整数
+		if(n<=9)return numbers1[n];
+		else if(10<=n && n<=19)return numberTeen[n%10];
+		else{
+			int shou=n/10;
+			int amari=n%10;
+			num=numbers10[shou]+"-"+numbers1[amari];
+			return num;
+		}
 	}
+	static String underThree(int n){//nは3桁の整数
+		if(n<=99)return underTwo(n);
+		else {
+			int shou100=n/100;
+			int amari100=n%100;//amari100は下二桁
+			num=numbers1[shou100]+" hundred and "+underTwo(amari100);
+			return num;
+		}
+	}
+
+
 }
