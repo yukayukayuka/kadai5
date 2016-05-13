03826b91e265613b7ca93905ccef5ca9316bacd5
g14954wy <g14954wy@s30424.tsuda.ac.jp>
2016-05-13 11:29:28 +0900

    変更前

---
+++ kadai5/src/kadai5/IntToEng.java
@@ -0,0 +1,16 @@
+package kadai5;
+
+import java.util.Scanner;
+
+public class IntToEng {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+        	int input = sc.nextInt();	
+        	
+        System.out.println(translateEng(input));
+        
+    }
+	static String translateEng(int n) {
+		return "";
+	}
+}
